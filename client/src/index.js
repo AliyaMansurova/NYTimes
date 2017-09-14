275cba375aa7ddc1243a8df44ef8073f5230a561
Aliya Mansurova <a11ya94@mail.ru>
2017-09-14 21:03:57 +0300

    webpack, package.json settings

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import './index.less';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/components/App/App.js';
+
+ReactDOM.render(
+<App />,
+    document.getElementById('app')
+);
---
+++ client/src/js/components/App/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './App.less';
+
+
+export default class App extends Component {
+  render() {
+    return ( <div>NYTimes</div>);
+  }
+}
